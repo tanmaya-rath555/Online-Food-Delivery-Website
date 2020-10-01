1579664a430141d899fb2d4f7567a17e29b337be
Tanmaya-Rath <72187265+tanmaya-rath555@users.noreply.github.com>
2020-10-01 17:35:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var typed_2 = new Typed('#typed_2' , {
+    strings: [
+        'Web Developer',
+        'Programmer',
+        
+        
+    ],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+});
